1e712aa5c841812fd8e97d259473e8abc008bb56
ffalt <ffalt@server.ffalt>
2013-06-01 18:42:53 +0200

    lobbyplag map: use group data

--- frontends/map/map.js
+++ frontends/map/map.js
@@ -13,6 +13,14 @@ for (var key in raw_countries) {
 	);
 }
 
+var raw_groups = JSON.parse(fs.readFileSync(path.resolve(__dirname, config.datadir, 'groups.json')));
+var groups = [];
+for (var key in raw_groups) {
+	groups.push(
+		{id: key, short: raw_groups[key].short, long: raw_groups[key].long}
+	);
+}
+
 /* configure Express */
 
 var app = express();
@@ -34,12 +42,10 @@ var tmpl = {
 	overview: fs.readFileSync(path.resolve(__dirname, "tmpl/overview.mustache")).toString(),
 	countries: fs.readFileSync(path.resolve(__dirname, "tmpl/countries.mustache")).toString(),
 	country: fs.readFileSync(path.resolve(__dirname, "tmpl/country.mustache")).toString(),
-	parties: fs.readFileSync(path.resolve(__dirname, "tmpl/parties.mustache")).toString(),
 	topics: fs.readFileSync(path.resolve(__dirname, "tmpl/topics.mustache")).toString(),
 	meps: fs.readFileSync(path.resolve(__dirname, "tmpl/meps.mustache")).toString(),
 	party: fs.readFileSync(path.resolve(__dirname, "tmpl/party.mustache")).toString(),
 
-	subnav_country: fs.readFileSync(path.resolve(__dirname, "tmpl/subnav_country.mustache")).toString(),
 	mail: fs.readFileSync(path.resolve(__dirname, "tmpl/mail.mustache")).toString()
 };
 
@@ -49,20 +55,24 @@ var sendTemplate = function (req, res, template, data) {
 	res.send(mustache.render(tmpl.index, data, {
 		"main": template,
 		"header": tmpl.header,
-		"footer": tmpl.footer,
-		subnav_country: tmpl.subnav_country  //todo only with corresponding template
+		"footer": tmpl.footer
 	}));
 	res.end();
 };
 
 var getParty = function (id) {
-	return {id: id, name: "mockup"};
+	for (var i = 0; i < groups.length; i++) {
+		if (groups[i].id === id) {
+			return groups[i];
+		}
+	}
+	return null;
 };
 
 var getCountry = function (id) {
 	for (var i = 0; i < countries.length; i++) {
 		if (countries[i].id === id) {
-			return      countries[i];
+			return countries[i];
 		}
 	}
 	return null;
@@ -84,32 +94,44 @@ app.get(config.prefix + '/countries/:country', function (req, res) {
 	if (!_country) {
 		sendTemplate(req, res, tmpl.countries, {"active_countries": true, countries: countries})
 	} else {
-		sendTemplate(req, res, tmpl.country, {"active_countries": true, countries: countries, country: _country})
+		var _countries = countries.map(function (country) {
+			return {id: country.id, name: country.name, active: country.id === _country.id};
+		});
+		sendTemplate(req, res, tmpl.country, {"active_countries": true, countries: _countries, country: _country})
 	}
 });
 
 app.get(config.prefix + '/parties', function (req, res) {
-	sendTemplate(req, res, tmpl.parties, {"active_parties": true});
+	var _party = groups[0];
+	var _parties = groups.map(function (party) {
+		return {id: party.id, short: party.short, long: party.long, active: (party.id === _party.id)};
+	});
+	var _countries = countries.map(function (country) {
+		return {id: country.id, name: country.name, party_id: _party.id};
+	});
+	sendTemplate(req, res, tmpl.party, {"active_parties": true, party: _party, parties: _parties, countries: _countries});
 });
 app.get(config.prefix + '/parties/:party', function (req, res) {
-	var _party = getParty(req.params.party);
-	if (!_party) {
-		sendTemplate(req, res, tmpl.parties, {"active_parties": true});
-	} else {
-		sendTemplate(req, res, tmpl.party, {"active_parties": true, party: _party});
-	}
+	var _party = getParty(req.params.party) || groups[0];
+	var _parties = groups.map(function (party) {
+		return {id: party.id, short: party.short, long: party.long, active: (party.id === _party.id)};
+	});
+	var _countries = countries.map(function (country) {
+		return {id: country.id, name: country.name, party_id: _party.id};
+	});
+	sendTemplate(req, res, tmpl.party, {"active_parties": true, party: _party, parties: _parties, countries: _countries});
 });
 
 app.get(config.prefix + '/parties/:party/:country', function (req, res) {
-	var _party = getParty(req.params.party);
+	var _party = getParty(req.params.party) || groups[0];
 	var _country = getCountry(req.params.country);
-	if (!_party) {
-		sendTemplate(req, res, tmpl.parties, {"active_parties": true});
-	} else if (!_country) {
-		sendTemplate(req, res, tmpl.party, {"active_parties": true, party: _party});
-	} else {
-		sendTemplate(req, res, tmpl.party, {"active_parties": true, party: _party, country: _country});
-	}
+	var _parties = groups.map(function (party) {
+		return {id: party.id, short: party.short, long: party.long, active: (party.id === _party.id)};
+	});
+	var _countries = countries.map(function (country) {
+		return {id: country.id, name: country.name, party_id: _party.id, active: ((_country) && (country.id === _country.id))};
+	});
+	sendTemplate(req, res, tmpl.party, {"active_parties": true, party: _party, parties: _parties, country: _country, countries: _countries});
 });
 
 app.get(config.prefix + '/topics', function (req, res) {
